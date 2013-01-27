969fa3577aac6d3631ca045ce6344b6c3a224d0b
kotakahashi <ec1513079@gmail.com>
2013-01-27 19:44:16 +0900

    Impl : 2dbook viewer

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
 
-function thumbnailsTag(base_url, title) {
+function thumbnailsTag(title, img_s, img_l, link_url) {
 	return '<li class="span4">' +
 	'<div class="thumbnail">' +
-	'<a href="' + base_url + '" class="thumbnail">' +
+	'<a href="' + link_url + '" class="thumbnail" target="_blank">' +
 	'<p class="title">' + title + '</p>' +
-	'<img class="t_img" alt="" src="' + base_url + '/t.jpg' + '" style=" ">' +
-	'<img class="p_img" alt="" src="' + base_url + '/p.jpg' + '" style="">' +
+	'<img class="t_img" alt="" src="' + img_s + '" style=" ">' +
+	'<img class="p_img" alt="" src="' + img_l + '" style="">' +
 	'</a>' +
 	'</div>' +
 	'</li>';
 }
 
+function thumbnailsTagForExploader(base_url, title) {
+	return thumbnailsTag(title, base_url + '/t.jpg', base_url + '/p.jpg', base_url);
+}
+
+function thumbnailsTagFor2dbook(base_url, title, link_url) {
+	return thumbnailsTag(title, base_url, base_url.replace('thumb.2dbook.com/thumb/', 'thumb.2dbook.com/thumb_img/'), link_url);
+}
+
 $(document).ready(function(){
 	
-	var xhr_ = new XMLHttpRequest();
-    xhr_.open("GET", "http://www.exploader.net/", true);
-    xhr_.onreadystatechange = function() {
+	var xhr_exp = new XMLHttpRequest();
+    xhr_exp.open("GET", "http://www.exploader.net/", true);
+    xhr_exp.onreadystatechange = function() {
 
-    	  if (xhr_.readyState == 4) {
-    	    var resp_ = $.parseHTML(xhr_.responseText);
+    	  if (xhr_exp.readyState == 4) {
+    	    var resp_ = $.parseHTML(xhr_exp.responseText);
 			$(resp_).find("div.list_inner p.info_title a").each(function(){
 				var link_  = $(this).attr("href");
 				var title_ = $(this).text();
-				$("ul#thumbnails_ul").append(thumbnailsTag(link_, title_));
+				$("#exploader_container > ul#thumbnails_ul").append(thumbnailsTagForExploader(link_, title_));
 			});
+			
+			$('[data-spy="scroll"]').each(function () {
+				var $spy = $(this).scrollspy('refresh');
+			});
+			$("li#nav_exp_li").addClass("active");
+			$("li#nav_2db_li").removeClass("active");
     	}
     };
-    xhr_.send();
+    
+	var xhr_2db = new XMLHttpRequest();
+    xhr_2db.open("GET", "http://2dbook.com/", true);
+    xhr_2db.onreadystatechange = function() {
+
+    	  if (xhr_2db.readyState == 4) {
+    	    var resp_ = $.parseHTML(xhr_2db.responseText);
+			$(resp_).find("div.content_filelist > div.itembox > div.item").each(function(){
+				var file_title_a = $(this).find("p.filetitle a");
+				var link_  = "http://2dbook.com" + file_title_a.attr("href");
+				var title_ = file_title_a.text();
+				var thumb_img_url_ = $(this).find("div.thumb > a > img").attr("src");
+				$("#2dbook_container > ul#thumbnails_ul").append(thumbnailsTagFor2dbook(thumb_img_url_, title_));
+			});
+			
+			$('[data-spy="scroll"]').each(function () {
+				var $spy = $(this).scrollspy('refresh');
+			});
+			$("li#nav_exp_li").addClass("active");
+			$("li#nav_2db_li").removeClass("active");
+    	}
+    };
+    
+    xhr_exp.send();
+    xhr_2db.send();
 });
